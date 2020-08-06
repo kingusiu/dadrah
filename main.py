b703557d59fda47c1a7d614342f3b785cc59ec68
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-06 17:44:10 +0200

    adding loss_strategy file with 5 loss combinations strategies; subclassing flat-cut-discriminator from discriminator

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ qcd_sig_id = 'qcdSigReco'
 experiment = ex.Experiment(run_n)
 data_img_vae = sf.read_results_to_jet_sample_dict([qcd_sig_id], experiment, mode='img-local')
 
+
+# split into discriminator train and test-set
+qcd_sig_train, qcd_sig_test = ...
+
+
+# train selector (and plot results => TODO)
+selector = ...
+selector.train(qcd_sig_train)
+
+# apply selection to datasample
+qcd_sig_train_selected = selector.apply(qcd_sig_train)
+qcd_sig_test_selected = selector.apply(qcd_sig_test)
+
+# plot mjj qcd sig to check for flat ratio
+
+
+# plot mjj accepted vs rejected signal to check for mass sculpting
+
+# do statistical analysis
--- selection/discriminator.py
+++ selection/discriminator.py
@@ -1,5 +1,21 @@
+import numpy as np
+
 
 class Discriminator():
 
-	pass
+	def __init__(self, loss_strategy):
+		self.loss_strategy = loss_strategy
+
+
+	def fit(self, jet_sample):
+		pass
+
+	def apply(self, jet_sample):
+		pass
+
+
+class FlatCutDiscriminator(Discriminator):
+
+	def fit(self, jet_sample):
+		loss = self.loss_strategy(jet_sample)
 	
