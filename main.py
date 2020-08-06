7e8c48ec277b6388e56e2a754243d85bbfb2a9e1
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-06 18:18:52 +0200

    implemented select() in flatcutDiscriminator; main: calling fit and select on qcd data (output not tested => TODO)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pofah.jet_sample as js
 import pofah.util.sample_factory as sf
 import pofah.util.experiment as ex
+import selection.discriminator as dis
+import selection.loss_strategy as ls
 
 
 # read in qcd signal region sample
@@ -8,20 +10,24 @@ run_n = 101
 qcd_sig_id = 'qcdSigReco'
 
 experiment = ex.Experiment(run_n)
-data_img_vae = sf.read_results_to_jet_sample_dict([qcd_sig_id], experiment, mode='img-local')
+qcd_sig_data = sf.read_results_to_jet_sample_dict([qcd_sig_id], experiment)
+
 
 
 # split into discriminator train and test-set
-qcd_sig_train, qcd_sig_test = ...
 
+# define quantile and loss-strategy for discimination
+quantile = 0.05 # 5%
+strategy = ls.combine_loss_min
 
+print(qcd_sig_data['qcdSigReco'].features())
 # train selector (and plot results => TODO)
-selector = ...
-selector.train(qcd_sig_train)
+selector = dis.FlatCutDiscriminator(quantile=quantile, loss_strategy=strategy)
+selector.fit(qcd_sig_data['qcdSigReco'])
 
 # apply selection to datasample
-qcd_sig_train_selected = selector.apply(qcd_sig_train)
-qcd_sig_test_selected = selector.apply(qcd_sig_test)
+qcd_sig_train_selected = selector.select(qcd_sig_data['qcdSigReco'])
+#qcd_sig_test_selected = selector.apply(qcd_sig_test)
 
 # plot mjj qcd sig to check for flat ratio
 
--- selection/discriminator.py
+++ selection/discriminator.py
@@ -3,14 +3,15 @@ import numpy as np
 
 class Discriminator():
 
-	def __init__(self, loss_strategy):
+	def __init__(self, quantile, loss_strategy):
 		self.loss_strategy = loss_strategy
+		self.quantile = quantile
 
 
 	def fit(self, jet_sample):
 		pass
 
-	def apply(self, jet_sample):
+	def select(self, jet_sample):
 		pass
 
 
@@ -18,4 +19,8 @@ class FlatCutDiscriminator(Discriminator):
 
 	def fit(self, jet_sample):
 		loss = self.loss_strategy(jet_sample)
-	
+		self.cut = np.percentile( loss, (1.-self.quantile)*100 )
+		
+	def select(self, jet_sample):
+		loss = self.loss_strategy(jet_sample)
+		return loss > self.cut
