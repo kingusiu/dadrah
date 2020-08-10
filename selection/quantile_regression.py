77f1d4f88f866001e8719f80f83edab59342b03d
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-10 14:25:26 +0200

    @QR: instance variable num_nodes_per_layer, default=100

--- selection/quantile_regression.py
+++ selection/quantile_regression.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 
 class QuantileRegression():
 
-	def __init__(self, quantile):
+	def __init__(self, quantile, num_nodes=100):
 		self.quantile = quantile
-
+		self.num_nodes_per_layer = num_nodes
 
 	def quantile_loss( self ):
 		def loss( target, pred ):
@@ -16,12 +16,12 @@ class QuantileRegression():
 
 
 	def build(self):
-		num_nodes_per_layer = 100
+		
 		self.inputs = tf.keras.Input(shape=(1,))
-		x = tf.keras.layers.Dense(num_nodes_per_layer, activation='relu')(self.inputs)
-		x = tf.keras.layers.Dense(num_nodes_per_layer, activation='relu')(x)
-		x = tf.keras.layers.Dense(num_nodes_per_layer, activation='relu')(x)
-		x = tf.keras.layers.Dense(num_nodes_per_layer, activation='relu')(x)
+		x = tf.keras.layers.Dense(self.num_nodes_per_layer, activation='relu')(self.inputs)
+		x = tf.keras.layers.Dense(self.num_nodes_per_layer, activation='relu')(x)
+		x = tf.keras.layers.Dense(self.num_nodes_per_layer, activation='relu')(x)
+		x = tf.keras.layers.Dense(self.num_nodes_per_layer, activation='relu')(x)
 		self.output = tf.keras.layers.Dense(1)(x)
 		model = tf.keras.Model(self.inputs, self.output)
 		model.compile(loss=self.quantile_loss(), optimizer='Adam') # Adam(lr=1e-4) TODO: add learning rate
