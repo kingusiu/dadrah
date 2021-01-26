fixing custom object in quantile regression load for normalization layers, other minor fixes

analysis/analysis_discriminator.py
@@ -13,9 +13,9 @@ import anpofah.util.plotting_util as pu
 def analyze_multi_quantile_discriminator_cut(discriminator_list, sample, feature_key='mJJ', plot_name='multi_discr_cut', fig_dir=None):
     fig = plt.figure(figsize=(8, 8))
     x_min = np.min(sample[feature_key])
-    x_max = np.percentile(sample[feature_key], 1e2*(1-1e-4))
+    x_max = np.percentile(sample[feature_key], 1e2*(1-1e-3))
     loss = discriminator_list[0].loss_strategy(sample)
-    plt.hist2d(sample[feature_key], loss, range=((x_min , x_max), (np.min(loss), np.percentile(loss, 1e2*(1-1e-4)))), norm=LogNorm(), bins=200)
+    plt.hist2d(sample[feature_key], loss, range=((x_min , x_max), (np.min(loss), np.percentile(loss, 1e2*(1-1e-3)))), norm=LogNorm(), bins=200)
     xs = np.arange(x_min, x_max, 0.001*(x_max-x_min))
     for discriminator in discriminator_list:
         plt.plot(xs, discriminator.predict( xs ) , '-', lw=2.5, label='cut Q'+str(discriminator.quantile*100))

selection/discriminator.py
@@ -6,6 +6,7 @@ import sklearn.ensemble as scikit
 import dadrah.selection.quantile_regression as qr
 import pofah.jet_sample as js
 import vande.training as train
+import vande.vae.layers as layers
 
 
 class Discriminator(metaclass=ABCMeta):
@@ -139,7 +140,7 @@ class QRDiscriminator(Discriminator):
         self.model.save(path)
 
     def load(self, path):
-        self.model = tf.keras.models.load_model(path, custom_objects={'MinMaxNormalization': qr.MinMaxNormalization, 'MinMaxUnnormalization': qr.MinMaxUnnormalization}, compile=False)
+        self.model = tf.keras.models.load_model(path, custom_objects={'StdNormalization': layers.StdNormalization, 'StdUnnormalization': layers.StdUnnormalization}, compile=False)
         print('loaded model ', self.model)
 
     def predict(self, data):
@@ -163,11 +164,9 @@ class QRDiscriminator_KerasAPI(QRDiscriminator):
     def fit(self, train_sample, valid_sample):
         # prepare training set
         (x_train, y_train), (x_valid, y_valid) = self.make_training_datasets(train_sample, valid_sample)
-        #self.set_mean_std_input_output(x_train, y_train)
 
-        self.model = qr.QuantileRegression(quantile=self.quantile, **self.model_params).build()
-        # import ipdb; ipdb.set_trace()
-        self.history = self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_sz, verbose=2, validation_data=(x_valid, y_valid)) #, shuffle=True, \
+        self.model = qr.QuantileRegression(quantile=self.quantile, x_mu_std=(np.mean(x_train), np.std(x_train)), **self.model_params).build()
+        self.history = self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_sz, verbose=2, validation_data=(x_valid, y_valid), shuffle=True) #, \
             # callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=1), tf.keras.callbacks.ReduceLROnPlateau(factor=0.2, patience=3, verbose=1)])
        
         return self.history.history['loss'], self.history.history['val_loss']

selection/quantile_regression.py
@@ -3,39 +3,39 @@ import vande.vae.layers as layers
 
 
 # ******************************************** #
-#			quantile regression loss 		   #
+#           quantile regression loss           #
 # ******************************************** #
 
 def quantile_loss(quantile):
-	@tf.function
+    @tf.function
     def loss(target, pred):
         err = target - pred
         return tf.where(err>=0, quantile*err, (quantile-1)*err)
     return loss
 
 # ******************************************** #
-#			quantile regression models 		   #
+#           quantile regression models         #
 # ******************************************** #
 
 
 class QuantileRegression():
 
-	def __init__(self, quantile, n_layers=5, n_nodes=20, x_mu_std=(0.,1.), optimizer='adam', initializer='he_uniform', activation='elu'):
-		self.quantile = quantile
-		self.n_layers = n_layers
-		self.n_nodes = n_nodes
-		self.x_mu_std = x_mu_std
-		self.optimizer = optimizer
-		self.initializer = initializer
-		self.activation = activation
+    def __init__(self, quantile, n_layers=5, n_nodes=20, x_mu_std=(0.,1.), optimizer='adam', initializer='he_uniform', activation='elu'):
+        self.quantile = quantile
+        self.n_layers = n_layers
+        self.n_nodes = n_nodes
+        self.x_mu_std = x_mu_std
+        self.optimizer = optimizer
+        self.initializer = initializer
+        self.activation = activation
 
-	def build(self):
-		inputs = tf.keras.Input(shape=(1,))
-    	x = layers.StdNormalization(*self.x_mu_std)(inputs)
-    	for _ in range(self.n_layers):
-        	x = tf.keras.layers.Dense(self.n_nodes, kernel_initializer=self.initializer, activation=self.activation)(x)
-    	outputs = tf.keras.layers.Dense(1, kernel_initializer=self.initializer)(x)
-		model = tf.keras.Model(inputs, output)
-		model.compile(loss=quantile_loss(self.quantile), optimizer=self.optimizer) # Adam(lr=1e-3) TODO: add learning rate
-		model.summary()
-		return model
+    def build(self):
+        inputs = tf.keras.Input(shape=(1,))
+        x = layers.StdNormalization(*self.x_mu_std)(inputs)
+        for _ in range(self.n_layers):
+            x = tf.keras.layers.Dense(self.n_nodes, kernel_initializer=self.initializer, activation=self.activation)(x)
+        outputs = tf.keras.layers.Dense(1, kernel_initializer=self.initializer)(x)
+        model = tf.keras.Model(inputs, outputs)
+        model.compile(loss=quantile_loss(self.quantile), optimizer=self.optimizer) # Adam(lr=1e-3) TODO: add learning rate
+        model.summary()
+        return model