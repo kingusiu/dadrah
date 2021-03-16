60a9952a62c5ca9d042f2c99f1f69695414bb186
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-03-16 17:10:28 +0100

    updating selection mjj spectrum plotting to use pofah::experiment paths for output; deleting redundant util/path_constants

--- analysis/analysis_selections.py
+++ analysis/analysis_selections.py
@@ -3,7 +3,7 @@ import dadrah.analysis.root_plotting_util as rpu
 import pofah.util.sample_factory as sf
 import pofah.jet_sample as js
 import pofah.util.utility_fun as utfu
-import dadrah.util.path_constants as paco
+import pofah.experiment as exp
 
 import pathlib
 
@@ -24,16 +24,15 @@ def plot_mjj_spectrum(sample, quantile, fig_dir='fig'):
 if __name__ == '__main__':
     
     run = 113
-    sample_ids = ['qcdSigAllTestReco', 'GtoWW35naReco']
+    sample_ids = ['qcdSigAllTestReco', 'GtoWW35brReco']
     sig_xsec = 100.
     quantiles = [0.1, 0.3, 0.5, 0.7, 0.9, 0.99]
     # quantiles = [0.9]
     mjj_key = 'mJJ'
+    param_dict = {'$run$': str(run), '$sig_name$': sample_ids[1], '$sig_xsec$': str(int(sig_xsec))}
 
-    input_paths = sf.SamplePathDirFactory(sdfs.path_dict).update_base_path({'$run$': str(run), '$sig_name$': sample_ids[1], '$sig_xsec$': str(int(sig_xsec))}) # in selection paths new format with run_x, sig_x, ...
-    fig_dir = utfu.multi_replace(paco.analysis_paths['mJJ_spectra'], {'$run$' : str(run)})
-    pathlib.Path(fig_dir).mkdir(parents=True, exist_ok=True)            
-
+    input_paths = sf.SamplePathDirFactory(sdfs.path_dict).update_base_path(param_dict) # in selection paths new format with run_x, sig_x, ...
+    fig_dir = exp.Experiment(run_n=params.run_n, param_dict=param_dict).setup(analysis_dir_qr=True).analysis_dir_qr_mjj
 
     for sample_id in sample_ids:
         for quantile in quantiles:
--- util/path_constants.py
+++
@@ -1,6 +0,0 @@
-# path constants for analysis output plots
-analysis_paths = {
-
-    'qr_cuts' : '/eos/user/k/kiwoznia/data/QR_results/analysis/run_$run$/qr_cuts',
-    'mJJ_spectra': '/eos/user/k/kiwoznia/data/QR_results/analysis/run_$run$/mjj_spectra'
-}
