20abff5e9ef3bb7d12bf60f407dbd156bc52b20d
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-05-10 10:55:00 +0200

    adding model str setting option in save qr and load qr routines [qr_workflow]

--- selection/qr_workflow.py
+++ selection/qr_workflow.py
@@ -22,17 +22,17 @@ def train_QR(quantile, mixed_train_sample, mixed_valid_sample, params, plot_loss
     return discriminator
 
 
-def save_QR(params, experiment, quantile, xsec):
+def save_QR(params, experiment, quantile, xsec, model_str=None):
     # save the model   
-    model_str = stco.make_qr_model_str(experiment.run_n, quantile, params.sig_sample_id, xsec, params.strategy_id)
+    model_str = model_str or stco.make_qr_model_str(experiment.run_n, quantile, params.sig_sample_id, xsec, params.strategy_id)
     model_path = os.path.join(experiment.model_dir_qr, model_str)
     discriminator.save(model_path)
     print('saving model {} to {}'.format(model_str, experiment.model_dir_qr))
     return model_path
 
 
-def load_QR(params, experiment, quantile, xsec, date):
-    model_str = stco.make_qr_model_str(experiment.run_n, quantile, params.sig_sample_id, sig_xsec=xsec, strategy_id=params.strategy_id, date=date)
+def load_QR(params, experiment, quantile, xsec, date, model_str=None):
+    model_str = model_str or stco.make_qr_model_str(experiment.run_n, quantile, params.sig_sample_id, sig_xsec=xsec, strategy_id=params.strategy_id, date=date)
     model_path = os.path.join(experiment.model_dir_qr, model_str)
     discriminator = disc.QRDiscriminator_KerasAPI(quantile=quantile, loss_strategy=lost.loss_strategy_dict[params.strategy_id], batch_sz=256, epochs=params.epochs,  n_layers=5, n_nodes=60)
     discriminator.load(model_path)
