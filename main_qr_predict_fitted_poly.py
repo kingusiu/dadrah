b716814d8449424541222b149bb2af42a6cc8f94
kingusiu <kinga.wozniak@gmail.com>
2021-08-20 20:41:31 +0200

    adding fitted qr cut notebook for 3-param and 5-param fits

--- main_qr_predict_fitted_poly.py
+++ main_qr_predict_fitted_poly.py
@@ -16,18 +16,26 @@ import pofah.phase_space.cut_constants as cuts
 #       fitted polynomial selections
 #****************************************#
 
-cut_polys = {
+cut_polys_par3 = {
     
     0.99 : np.poly1d([-7.03032980e-11, 1.06542864e-06, -2.17139299e-03, 3.31447885e+00]),
     0.9 : np.poly1d([-4.60568395e-11, 7.63471757e-07, -1.55537204e-03, 2.81116723e+00]),
     0.1 : np.poly1d([-1.57031061e-11, 3.01628110e-07, -5.03564141e-04, 1.83966064e+00])
 }
 
-def fitted_selection(sample, strategy_id, quantile):
+cut_polys_par5 = {
+    
+    0.99 : np.poly1d([6.39477740e-18, -1.32096520e-13, 9.60377076e-10, -2.68629790e-06, 4.11271170e-03, -5.10765776e-01]),
+    0.9 : np.poly1d([4.53587326e-18, -9.43590874e-14, 6.95373175e-10, -1.95383392e-06, 3.02479614e-03, 8.16766148e-03]),
+    0.1 : np.poly1d([1.43916815e-18, -3.34572370e-14, 2.74613208e-10, -8.59156457e-07, 1.60246515e-03, 4.73345295e-01])
+}
+
+def fitted_selection(sample, strategy_id, quantile, params_n=5):
     loss_strategy = lost.loss_strategy_dict[strategy_id]
     loss = loss_strategy(sample)
-    loss_cut = cut_polys[quantile]
+    loss_cut = cut_polys_par5[quantile] if params_n == 5 else cut_polys_par3[quantile]
     return loss > loss_cut(sample['mJJ'])
+
 
 
 
@@ -45,7 +53,7 @@ quantiles = [0.1, 0.9, 0.99]
 
 
 
-Parameters = recordtype('Parameters','run_n, qcd_sample_id, qcd_ext_sample_id, qcd_train_sample_id, qcd_test_sample_id, sig_sample_id, strategy_id, epochs, read_n')
+Parameters = recordtype('Parameters','run_n, qcd_sample_id, qcd_ext_sample_id, qcd_train_sample_id, qcd_test_sample_id, sig_sample_id, strategy_id, read_n')
 params = Parameters(run_n=113, 
                     qcd_sample_id='qcdSigReco', 
                     qcd_ext_sample_id='qcdSigExtReco',
@@ -56,15 +64,11 @@ params = Parameters(run_n=113,
                     read_n=None)
 
 
-#****************************************#
-#           read in qcd data
-#****************************************#
-
 paths = sf.SamplePathDirFactory(sdfr.path_dict).update_base_path({'$run$': 'run_'+str(params.run_n)})
-qcd_test_sample = js.JetSample.from_input_dir(params.qcd_test_sample_id, paths.sample_dir_path(params.qcd_test_sample_id), read_n=params.read_n)
 
 #****************************************#
-#      for each signal: QR & dijet fit
+#      for qcd and each signal: 
+#   read data, make selection and dump
 #****************************************#
 
 for sample_id in [params.qcd_test_sample_id] + signals:
@@ -81,7 +85,7 @@ for sample_id in [params.qcd_test_sample_id] + signals:
         inv_quant = round((1.-quantile),2)
 
         #print('predicting {}'.format(sample.name))
-        selection = fitted_selection(sample, strategy_id, quantile)
+        selection = fitted_selection(sample, params.strategy_id, quantile)
         sample.add_feature('sel_q{:02}'.format(int(inv_quant*100)), selection)
 
         # write results for all quantiles
