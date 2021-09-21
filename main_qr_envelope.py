30c3bd15fabd257bd6999a6befe5bc44d7e10dc6
kingusiu <kinga.wozniak@gmail.com>
2021-09-21 13:49:16 +0200

    restoring poly fit script

--- main_qr_envelope.py
+++ main_qr_envelope.py
@@ -14,6 +14,8 @@ import dadrah.util.string_constants_util as stco
 import dadrah.selection.qr_workflow as qrwf
 import pofah.util.experiment as ex
 import analysis.analysis_discriminator as andi
+import pofah.jet_sample as js
+import pofah.phase_space.cut_constants as cuts
 
 
 def slice_datasample_n_parts(data, parts_n):
@@ -27,8 +29,7 @@ def inv_quantile_str(quantile):
 
 
 # setup runtime params and csv file
-#quantiles = [0.1, 0.9, 0.99]
-quantiles = [0.3, 0.5, 0.7]
+quantiles = [0.1, 0.3, 0.5, 0.7, 0.9, 0.99]
 parts_n = 5
 bin_edges = np.array([1200, 1255, 1320, 1387, 1457, 1529, 1604, 1681, 1761, 1844, 1930, 2019, 2111, 2206, 
                         2305, 2406, 2512, 2620, 2733, 2849, 2969, 3093, 3221, 3353, 3490, 3632, 3778, 3928, 
@@ -36,12 +37,14 @@ bin_edges = np.array([1200, 1255, 1320, 1387, 1457, 1529, 1604, 1681, 1761, 1844
 bin_centers = [(high+low)/2 for low, high in zip(bin_edges[:-1], bin_edges[1:])]
 print('bin centers: ', bin_centers)
 
-Parameters = recordtype('Parameters','run_n, qcd_sample_id, qcd_ext_sample_id, qcd_train_sample_id, qcd_test_sample_id, strategy_id, epochs, read_n')
+Parameters = recordtype('Parameters','run_n, qcd_sample_id, qcd_ext_sample_id, qcd_train_sample_id, qcd_test_sample_id, sig_sample_id, sig_xsec, strategy_id, epochs, read_n')
 params = Parameters(run_n=113, 
                     qcd_sample_id='qcdSigReco', 
                     qcd_ext_sample_id='qcdSigExtReco',
                     qcd_train_sample_id='qcdSigAllTrainReco', 
                     qcd_test_sample_id='qcdSigAllTestReco',
+                    sig_sample_id='GtoWW35brReco',
+                    sig_xsec=100,
                     strategy_id='rk5_05',
                     epochs=100,
                     read_n=None)
@@ -53,15 +56,25 @@ pathlib.Path(experiment.model_dir_qr).mkdir(parents=True, exist_ok=True)
 result_dir = '/eos/user/k/kiwoznia/data/QR_results/analysis/run_' + str(params.run_n) + '/envelope'
 pathlib.Path(result_dir).mkdir(parents=True, exist_ok=True)            
 
+
 #****************************************#
 #           read in qcd data
 #****************************************#
 paths = sf.SamplePathDirFactory(sdfr.path_dict).update_base_path({'$run$': 'run_'+str(params.run_n)})
 
-data_qcd_all = dapr.merge_qcd_base_and_ext_datasets(params, paths)
+data_qcd_all = dapr.merge_qcd_base_and_ext_datasets(params, paths, **cuts.signalregion_cuts)
 print('qcd all: min mjj = {}, max mjj = {}'.format(np.min(data_qcd_all['mJJ']), np.max(data_qcd_all['mJJ'])))
 # split qcd data
 data_qcd_parts = slice_datasample_n_parts(data_qcd_all, parts_n)
+
+#****************************************#
+#           signal injection             #
+
+signal_str = params.sig_sample_id[:-4] or 'no_signal'
+sig_in_training_n = 1113
+sig_sample = js.JetSample.from_input_dir(params.sig_sample_id, paths.sample_dir_path(params.sig_sample_id), read_n=params.read_n, **cuts.signalregion_cuts) 
+
+
 
 cut_results = {}
 
@@ -79,15 +92,22 @@ for quantile in quantiles:
 
     # for each qcd data part
     for dat_train, dat_valid, model_n in zip(data_qcd_parts, data_qcd_parts[1:] + [data_qcd_parts[0]], list('ABCDE')):
+
+        # inject signal
+        sig_train = sig_sample.sample(int(sig_in_training_n/parts_n))
+        sig_valid = sig_sample.sample(int(sig_in_training_n/parts_n))
+        dat_train = dat_train.merge(sig_train)
+        dat_valid = dat_valid.merge(sig_valid)
+
         # train qr
         print('training on {} events, validating on {}'.format(len(dat_train), len(dat_valid)))
         discriminator = qrwf.train_QR(quantile, dat_train, dat_valid, params)
         models.append(discriminator)
 
         # save qr
-        model_str = stco.make_qr_model_str(experiment.run_n, quantile, 'no_signal', 0, params.strategy_id)
+        model_str = stco.make_qr_model_str(experiment.run_n, quantile, signal_str, params.sig_xsec, params.strategy_id)
         model_str = model_str[:-3] + '_' + model_n + model_str[-3:]
-        discriminator_path = qrwf.save_QR(discriminator, params, experiment, quantile, 0, model_str)
+        discriminator_path = qrwf.save_QR(discriminator, params, experiment, quantile, params.sig_xsec, model_str)
 
         # predict cut values per bin
         cuts_part = discriminator.predict(bin_centers)
@@ -109,10 +129,11 @@ for quantile in quantiles:
     cut_results.update({inv_quantile_str(quantile): cuts_for_quantile.tolist()})
 
     # plot quantile cut bands
-    title_suffix = ' 5 models trained qcd SR no signal q ' + 'q{:02}'.format(int(quantile*100))
-    plot_name = 'multi_discr_cut_no_signal_5models_' + 'q{:02}'.format(int(quantile*100))
+    title_suffix = ' 5 models trained qcd SR ' + signal_str + ' q ' + 'q{:02}'.format(int(quantile*100))
+    plot_name = 'multi_discr_cut_' + signal_str + '_5models_' + 'q{:02}'.format(int(quantile*100))
     andi.analyze_multi_quantile_discriminator_cut(models, dat_valid, title_suffix=title_suffix, plot_name=plot_name, fig_dir=result_dir)
 
 # write cut result json file
-with open(os.path.join(result_dir, 'cut_stats_q357.json'), 'w') as ff:
+json_name = 'cut_stats_allQ_'+ signal_str +'.json'
+with open(os.path.join(result_dir, json_name), 'w') as ff:
     json.dump(cut_results, ff)
---
+++ main_qr_predict_fitted_poly.py
@@ -0,0 +1,106 @@
+import numpy as np
+from recordtype import recordtype
+import os
+
+import pofah.jet_sample as js
+import pofah.util.sample_factory as sf
+import pofah.util.experiment as ex
+import pofah.path_constants.sample_dict_file_parts_reco as sdfr
+import pofah.path_constants.sample_dict_file_parts_selected as sdfs
+import dadrah.selection.loss_strategy as lost
+import dadrah.util.data_processing as dapr
+import pofah.phase_space.cut_constants as cuts
+
+
+
+#****************************************#
+#       fitted polynomial selections
+#****************************************#
+
+# 3-param fit
+
+cut_polys_par3 = {
+    
+    0.99 : np.poly1d([-7.03032980e-11, 1.06542864e-06, -2.17139299e-03, 3.31447885e+00]),
+    0.9 : np.poly1d([-4.60568395e-11, 7.63471757e-07, -1.55537204e-03, 2.81116723e+00]),
+    0.1 : np.poly1d([-1.57031061e-11, 3.01628110e-07, -5.03564141e-04, 1.83966064e+00])
+}
+
+# 5-param-fit
+
+cut_polys_par5 = {
+    0.99 : np.poly1d([-4.16600751e-18, 4.49261774e-14, -1.60506482e-10, 6.43707025e-07, -5.03695064e-04, 1.87157225e+00]),
+    0.9 : np.poly1d([-3.27587188e-18, 3.57458088e-14, -1.17939773e-10, 4.22470378e-07, -2.11205166e-04, 1.65011401e+00]),
+    0.1 : np.poly1d([-8.34423542e-19, -2.39966921e-16, 8.92683714e-11, -3.67360323e-07, 9.83976050e-04, 7.67849195e-01]),
+    0.7 : np.poly1d([ 4.56721509e-19, -1.29451508e-14,  1.21611625e-10, -2.08025414e-07, 5.29528854e-04,  1.26264581e+00]),
+    0.5 : np.poly1d([-7.77474839e-20, -8.90000193e-15,  1.18415519e-10, -2.86784107e-07, 6.98665912e-04,  1.11383712e+00]),
+    0.3 : np.poly1d([-1.18038765e-18,  4.05419442e-15,  6.37113446e-11, -2.27612202e-07, 7.11545714e-04,  1.02408896e+00])
+}
+
+
+def fitted_selection(sample, strategy_id, quantile, params_n=5):
+    loss_strategy = lost.loss_strategy_dict[strategy_id]
+    loss = loss_strategy(sample)
+    loss_cut = cut_polys_par5[quantile] if params_n == 5 else cut_polys_par3[quantile]
+    return loss > loss_cut(sample['mJJ'])
+
+
+
+#****************************************#
+#           run parameters
+#****************************************#
+
+# signals
+resonance = 'na'
+signals = ['GtoWW15'+resonance+'Reco', 'GtoWW25'+resonance+'Reco', 'GtoWW35'+resonance+'Reco', 'GtoWW45'+resonance+'Reco']
+masses = [1500, 2500, 3500, 4500]
+xsec = 0.
+quantiles = [0.1, 0.9, 0.99, 0.3, 0.5, 0.7]
+params_n = 5
+
+
+
+Parameters = recordtype('Parameters','run_n, qcd_sample_id, qcd_ext_sample_id, qcd_train_sample_id, qcd_test_sample_id, sig_sample_id, strategy_id, read_n')
+params = Parameters(run_n=113, 
+                    qcd_sample_id='qcdSigReco', 
+                    qcd_ext_sample_id='qcdSigExtReco',
+                    qcd_train_sample_id='qcdSigAllTrainReco', 
+                    qcd_test_sample_id='qcdSigAllTestReco',
+                    sig_sample_id=None, # set sig id later in loop
+                    strategy_id='rk5_05',
+                    read_n=None)
+
+
+paths = sf.SamplePathDirFactory(sdfr.path_dict).update_base_path({'$run$': 'run_'+str(params.run_n)})
+
+#****************************************#
+#      for qcd and each signal: 
+#   read data, make selection and dump
+#****************************************#
+
+for sample_id in [params.qcd_test_sample_id] + signals:
+
+    sample = js.JetSample.from_input_dir(sample_id, paths.sample_dir_path(sample_id), **cuts.signalregion_cuts)
+
+    result_path = os.path.join('/eos/project/d/dshep/TOPCLASS/DijetAnomaly/QR_models/envelope/fitted_selections/run_113/xsec_0/loss_rk5_05', 'param'+str(params_n))
+
+    # param_dict = {'$sig_name$': sample_id, '$sig_xsec$': str(int(xsec)), '$loss_strat$': params.strategy_id}
+    # experiment = ex.Experiment(run_n=params.run_n, param_dict=param_dict).setup(model_dir_qr=True, analysis_dir_qr=True)
+    # result_paths = sf.SamplePathDirFactory(sdfs.path_dict).update_base_path({'$run$': str(params.run_n), **param_dict}) # in selection paths new format with run_x, sig_x, ...
+    # result_paths = result_paths.extend_base_path('fitted_cut', 'param'+str(params_n))
+
+    for quantile in quantiles:
+
+        # using inverted quantile because of dijet fit code
+        inv_quant = round((1.-quantile),2)
+
+        #print('predicting {}'.format(sample.name))
+        selection = fitted_selection(sample, params.strategy_id, quantile, params_n)
+        sample.add_feature('sel_q{:02}'.format(int(inv_quant*100)), selection)
+
+    # write results for all quantiles
+    file_path = os.path.join(result_path, sdfr.path_dict['file_names'][sample_id]+'.h5')
+    sample.dump(file_path)
+
+
+    
